1d5fdd341e33a5f759aa72870ba8b1b64817921d
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-06-17 21:24:53 +0800

    added simple echo server; init worker pool

--- scripts/echo.go
+++ scripts/echo.go
@@ -1,17 +1,10 @@
+// A simple echo server.
 package main
 
 import (
 	"log"
 	"net/http"
-	"runtime"
 )
-
-// Roughly estimate the capacity of the worker pool.
-var nworker int = runtime.NumCPU() * 2
-
-func worker(id int, jobs <-chan int, results chan<- int) {
-	// TODO
-}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world!\n"))
